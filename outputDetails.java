7adfb2e64b6a58a8a93204cc37be02ba6a22eb42
Patrick CYUBAHIRO <ampatrickcyubahiro@gmail.com>
2020-01-10 11:00:02 +0300

    Create outputDetails.java

---
+++ outputDetails.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Patrick CYUBAHIRO
+ */
+public class UserTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String FullName="Patrick CYUBAHIRO";
+        String Email="ampatrickcyubahiro@gmail.com";
+        int AccountNo= 10;
+        int ID= 3452356;
+        // TODO code application logic here
+        System.out.print("Your FullName is: "+FullName);
+        System.out.println("\nYour Email is: "+Email);
+        System.out.println("Your Account Number is: "+AccountNo);
+        System.out.println("Your ID number is: "+ID);
+
+    }
+}
